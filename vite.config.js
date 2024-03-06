6c20b39920bf7ab94a7f78730cb48bff563fec69
Hubert Ryan <hubert.ryan@nickelpay.com.br>
2024-03-06 01:22:49 -0300

    feat: npm configured

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.jsx"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "kova-render",
 
       fileName: "index",
